fff71f60de8aca63616a1203fa31dacb960d055e
iliangogov <iliangogov@gmail.com>
2017-01-16 18:49:14 +0200

    latest version applied

---
+++ 01. Introduction-to-ASP.NET/Homework/ASP.NET-intro/Sumator/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sumator.Startup))]
+namespace Sumator
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ 02. ASP.NET-Web-Forms-Intro/demos/Sumator/Sumator.aspx.cs
@@ -0,0 +1,23 @@
+﻿namespace Sumator
+{
+    using System;
+    using System.Globalization;
+
+    public partial class Sumator : System.Web.UI.Page
+    {
+        protected void ButtonCalculateSum_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var firstNum = decimal.Parse(this.TextBoxFirstNum.Text);
+                var secondNum = decimal.Parse(this.TextBoxSecondNum.Text);
+                var sum = firstNum + secondNum;
+                this.TextBoxSum.Text = sum.ToString(CultureInfo.InvariantCulture);
+            }
+            catch (Exception)
+            {
+                this.TextBoxSum.Text = "Invalid.";
+            }
+        }
+    }
+}
